01509d0c6ba1da812b5f8cf4a70198185d91fab8
Armon Castor <22066980+GlitchedCloud@users.noreply.github.com>
2025-09-29 22:36:28 +1000

    fix: add exposedHeaders to CORS configuration

--- src/config/index.js
+++ src/config/index.js
@@ -23,6 +23,7 @@ const config = {
     origin: process.env.ALLOWED_ORIGINS
       ? process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim())
       : false, 
+    exposedHeaders: ['X-Image-Source'],
     credentials: false
   },
 
